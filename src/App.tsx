aef0343e3a2aab0d22ead725bc038785f54f1bdb
kevinluo <luokev1@gmail.com>
2025-09-17 20:53:48 -0400

    crt distortion wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,28 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import TestBackground from './components/TestBackground'
 import './App.css'
+import CRTSceneR3F from './components/CRTShaderScene'
+import TransparentCRTOverlay from './components/CRTEffect'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <div>
+    <TestBackground />
+    <TransparentCRTOverlay
+        
+        opacity={0.55}
+        scanlineDensity={1500}
+        scanlineIntensity={0.14}
+        vignette={0.45}
+        noise={0.05}
+        flicker={0.02}
+        rgbOffset={1.0}
+      />
+   </div>
   )
 }
 
---
+++ src/components/portfolio/PortfolioPage.tsx
@@ -0,0 +1,7 @@
+
+
+const PortfolioPage = () => {
+    return <div>Portfolio Page</div>;
+};
+
+export default PortfolioPage;

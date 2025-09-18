daf4014a21035df19129792b66faaea9d43ce78c
kevinluo <luokev1@gmail.com>
2025-09-18 17:56:21 -0400

    removing unused components

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import TestBackground from './components/TestBackground'
 import './App.css'
-import CRTSceneR3F from './components/CRTShaderScene'
-import TransparentCRTOverlay from './components/CRTEffect'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <div>
-    <TestBackground />
-    <TransparentCRTOverlay
-        
-        opacity={0.55}
-        scanlineDensity={1500}
-        scanlineIntensity={0.14}
-        vignette={0.45}
-        noise={0.05}
-        flicker={0.02}
-        rgbOffset={1.0}
-      />
-   </div>
+ 
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
   )
 }
 

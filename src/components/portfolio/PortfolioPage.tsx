a8143e28991ff82a24edf79961490f535c98a050
kevinluo <luokev1@gmail.com>
2025-09-19 19:22:37 -0400

    experimentation with distortion effect

--- src/components/portfolio/PortfolioPage.tsx
+++ src/components/portfolio/PortfolioPage.tsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProjectCanvasDisplay from "./ProjectCanvasDisplay";
 import ProjectListContainer from "./ProjectListContainer";
 import { ProjectItem } from "../../models/ProjectItem";
 import ProjectId from "../../models/ProjectId";
 import GlitchWrap from "../effectWrappers/GlitchWrap";
 import CRTAberrationWrap from "../effectWrappers/GlitchWrap";
+// @ts-ignore: sketch.js has no type declarations
+import Sketch from '../../sketch.js'
 
 
 export const personalProjects: Map<ProjectId, ProjectItem> = new Map<ProjectId, ProjectItem>([
@@ -36,6 +38,12 @@ export const personalProjects: Map<ProjectId, ProjectItem> = new Map<ProjectId, 
 ]);
 
 const PortfolioPage = () => {
+
+    useEffect(() => {
+       new Sketch({
+             dom: document.getElementById("canvasContainer")
+        });
+    }, []);
     
     const dummyProject = new ProjectItem("Test Project", "This is a test project", null, null);
 
@@ -50,7 +58,7 @@ const PortfolioPage = () => {
         }
     }
 
-    return <div>
+    return <div id="canvasContainer" className="min-h-screen w-full flex flex-col items-center justify-center bg-gradient-to-b from-gray-900 via-gray-800 to-gray-900 text-white p-4">
         <CRTAberrationWrap
       intensity={1}     // wobble
       rgbOffset={5.0}     // strong channel split
